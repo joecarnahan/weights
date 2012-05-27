297e48a906cb7d6b71ee519b6177b88cb80ca9ec
Joe Carnahan <joseph.carnahan@gmail.com>
2012-05-27 09:48:45 +0100

    Now generates output for different minimum step sizes and sends output to different files

--- src/main/scala/weights/Main.scala
+++ src/main/scala/weights/Main.scala
@@ -7,9 +7,22 @@
 
 package weights
 
+import java.io.BufferedWriter
+import java.io.FileWriter
+
 object Main {
-  val minStep = 2.0
+  val minSteps = List(1.0, 2.0, 4.0)
 
   def main(args: Array[String]) = 
-    println((new Weights(minStep, args)).calculatePossibilities)
+    minSteps.map((minStep: Double) => {
+      val filename = "steps_" + minStep.toString + ".txt"
+      val writer = new BufferedWriter(new FileWriter(filename))
+      try {
+        writer.write((new Weights(minStep, args)).calculatePossibilities)
+        writer.newLine
+      }
+      finally {
+        writer.close
+      }
+    })
 }
